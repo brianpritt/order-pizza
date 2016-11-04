90222787624bf6d4ce1f60d767b24ea2eccae6f9
Brian Pritt <brianpritt@gmail.com>
2016-11-04 15:30:18 -0700

    add more styling and full funtionallity to page.  Delivery options and pick up both work.

--- js/scripts.js
+++ js/scripts.js
@@ -5,11 +5,14 @@ function PizzaOrder(size, crust, toppings, note, number ){
   this.note=note;
   this.number=number;
 }
+function Customer(name, address, phone){
+  this.name=name;
+  this.address=address;
+  this.phone=phone;
+}
 //one global variable.  Price!!!
 var price = 0;
 
-var toppingsVeg=["tomatoes","olives","onions","bell peppers","mushrooms"]
-var toppingsMeat=["Sausage","ham","peperoni","anchovies"]
 
 PizzaOrder.prototype.price = function(myPizza){
   var toppingPrice;
@@ -28,11 +31,9 @@ PizzaOrder.prototype.price = function(myPizza){
 $(document).ready(function(){//document.ready
 
   var toppingArray = [];
-  var size;
   var sizeInput = 0;
   var crustInput = 0;
-  var notes;
-  var numberOfPies;
+  var myPizza, myCustomer,numberOfPies, size, notes;
 
   $("#welcome .startOrder").click(function(){//starts the pizza order
     $("#place-order").slideDown();
@@ -49,10 +50,14 @@ $(document).ready(function(){//document.ready
       crustInput = $("#crust").val();
     if (sizeInput == 0){
       alert("Please select a size");
-    } else{
-      var myPizza = new PizzaOrder(sizeInput, crustInput, toppingArray, notes, numberOfPies);//creates pizza object
+    } else if (crustInput == 0){
+      alert("Please select a style of crust")
+    }  else{
+      myPizza = new PizzaOrder(sizeInput, crustInput, toppingArray, notes, numberOfPies);//creates pizza object
       $("#place-order").hide();
       $("#review-order").slideDown();
+      $(".pizza-size").append(myPizza.size + " ");
+      $(".pizza-crust").append(myPizza.crust + " ");
       $(".customer-toppings").text(myPizza.toppings);
       $(".price").text("Price: $" + myPizza.price(myPizza));
     }
@@ -60,20 +65,41 @@ $(document).ready(function(){//document.ready
 
   $(".place-order").click(function(){
     var deliverInput = $("#delivery").val();
-    if (deliverInput === "Delivery"){$("#deliver-input").show();}
-    else {
-    $("review-order").hide();
+    if (deliverInput ===  "Delivery"){$("#deliver-input").show();
+    debugger;
+
+    } else {
+    $("#review-order").hide();
+    $("#order-over").slideDown();
+    $(".pizza-size2").append(myPizza.size + " ");
+    $(".pizza-crust2").append(myPizza.crust + " ");
+    $(".customer-toppings2").append(", with " +myPizza.toppings);
+    $("which-one").append(" will be ready in 30 minutes!");
     }
   });
-
-  $(".confirm").submit(function(){
-      $(".review-order").hide();
-      $("#order-placed").show();
+  $("#confirm").click(function(event){
+    event.preventDefault();
+    var nameInput = $(".cust-name").val();
+    var addInput = $(".cust-add").val();
+    var phoneInput = $(".cust-phone").val();
+    myCustomer = new Customer(nameInput, addInput, phoneInput);
+    $("#deliver-input").hide();
+    $("#review-order").hide();
+    $("#order-over").slideDown();
+    $(".customer-name").append(myCustomer.name)
+    $(".pizza-crust2").append(myPizza.crust + " ");
+    $(".pizza-size2").append(myPizza.size + " ");
+    $(".customer-toppings2").append(", with " +myPizza.toppings);
+    $("#which-one").append(" will be delivered within 45 minutes!")
   });
 
   $(".change-order").click(function(){
     toppingArray = [];
     $("#place-order").slideDown();
     $("#review-order").hide();
+    $("#deliver-input").hide();
+
   });
+
+
 });
