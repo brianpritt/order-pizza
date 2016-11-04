87f89d4097569e106222047ff10910c11ed85e54
Brian Pritt <brianpritt@gmail.com>
2016-11-04 12:05:20 -0700

    add option for delivery and input fields associated with such

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,44 @@
-function PizzaOrder(size, toppings, note, number ){
+function PizzaOrder(size, crust, toppings, note, number ){
   this.size=size;
+  this.crust=crust;
   this.toppings= toppings;
   this.note=note;
   this.number=number;
 }
+//one global variable.  Price!!!
+var price = 0;
 
 var toppingsVeg=["tomatoes","olives","onions","bell peppers","mushrooms"]
 var toppingsMeat=["Sausage","ham","peperoni","anchovies"]
 
 PizzaOrder.prototype.price = function(myPizza){
-  var price = 0;
   var toppingPrice;
   if (myPizza.size === "Small"){price += 8}
   else if (myPizza.size === "Medium"){price += 10}
   else if (myPizza.size === "Large"){price += 12}
   else if (myPizza.size === "Xtra-large"){price += 14}
+
+  if (myPizza.crust === "Stuffed Crust"){price += 2}
+
   toppingPrice = (myPizza.toppings.length) *.5;
   price += toppingPrice;
   return price;
 }
 
-$(document).ready(function(){
+$(document).ready(function(){//document.ready
 
   var toppingArray = [];
   var size;
   var sizeInput = 0;
+  var crustInput = 0;
   var notes;
   var numberOfPies;
+
   $("#welcome .startOrder").click(function(){//starts the pizza order
     $("#place-order").slideDown();
     $("#welcome").hide();
   });
+
   $("form#toppings").submit(function(event){//submits the order for review
     event.preventDefault();
     $("input:checkbox[name=topping]:checked").each(function(){//puts toppings into an array
@@ -38,18 +46,34 @@ $(document).ready(function(){
       toppingArray.push(topping);
     });
       sizeInput = $("#size").val();
+      crustInput = $("#crust").val();
     if (sizeInput == 0){
       alert("Please select a size");
     } else{
-      var myPizza = new PizzaOrder(sizeInput, toppingArray, notes, numberOfPies);//creates pizza object
+      var myPizza = new PizzaOrder(sizeInput, crustInput, toppingArray, notes, numberOfPies);//creates pizza object
       $("#place-order").hide();
       $("#review-order").slideDown();
-      $(".customer-order").text(myPizza.size);
-      $(".price").text(myPizza.price(myPizza));
+      $(".customer-toppings").text(myPizza.toppings);
+      $(".price").text("Price: $" + myPizza.price(myPizza));
     }
   });
-  $("#place-order").click(function(){
-    $("review-order").hide();
 
+  $(".place-order").click(function(){
+    var deliverInput = $("#delivery").val();
+    if (deliverInput === "Delivery"){$("#deliver-input").show();}
+    else {
+    $("review-order").hide();
+    }
+  });
+
+  $(".confirm").submit(function(){
+      $(".review-order").hide();
+      $("#order-placed").show();
+  });
+
+  $(".change-order").click(function(){
+    toppingArray = [];
+    $("#place-order").slideDown();
+    $("#review-order").hide();
   });
 });
